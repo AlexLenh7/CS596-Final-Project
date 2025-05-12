569b53519e6c97a5f62fb2247c29e39f7809e603
AlexLenh7 <alexlenh666@gmail.com>
2025-05-11 21:37:55 -0700

    Merge branch 'main' of https://github.com/AlexLenh7/CS596-Final-Project

--- CS596 Final Project/Assets/Scripts/Rythm.cs
+++ CS596 Final Project/Assets/Scripts/Rythm.cs
@@ -6,21 +6,34 @@ public class Rythm : MonoBehaviour
     [SerializeField] private AudioClip TapSound;
     [Range(0f, 1f)]
     public float volume = 1f;
-    //These paramaters can be changed in the future, this is only an example for now
 
     //Terry: I am adding variables to maintain score, streaks, etc.
+    //Terry: Using the data collected from inputs, determine scores and conditions within this entire script
+    
     //Game manager will read these variables to determine win conditions.
-
     public float currHP = 100; //Heal this value depending on accuracy
     public float maxHP = 100; //MaxHP
     public float score = 0; //add to this score based on input accuracy data
     public float streak = 0; //add to this value as long as the player does not land a miss
-    
+
     //Amount of HP being subtracted 
     float dmgValue = .1f; //Should be taken from difficulty choice in the main menu's scriptable object
 
     //Frequency in seconds that you lose take the dmgValue
     float drainRate = .2f; //Should be taken from difficulty choice in the main menu's scriptable object
+
+    public Transform timingLine;
+
+    [SerializeField]
+    private float maxAllowedNoteDelta = 0.15f;
+
+    private int noteStartLane;
+    private float noteStartPos; //Y position
+
+    //IMPORTANT: These variables need to be populated somehow, the specific way to do so can be figured out once note spawning is complete
+    private Note lowestNote;
+    private float noteEndTime;
+    private float noteCurrPos;
 
     void Start()
     {
@@ -28,29 +41,72 @@ public class Rythm : MonoBehaviour
         StartCoroutine("dmgOverTime");
     }
 
-    //Terry: Using the data collected from inputs, determine scores and conditions within this entire script
-    public void BeginTouch(GameObject startLane)
-    {
-        //Collect information at beginning touch such as how far off player was from note and whatnot
-        SoundManager.instance.playSound(TapSound, transform, volume); // tap sound is called from soundmanager instance
-        Debug.Log("Touch began at lane " + startLane);
-    }
-
-    public void EndTouch(GameObject endLane)
-    {
-        //Now deal with earlier information, and check to see if touch was a hold, if so deal with it accordingly
-        Debug.Log("Touch ended at lane " + endLane);
-    }
-
-    //HP Drain
     IEnumerator dmgOverTime()
     {
         while (currHP > 0)
         {
             currHP -= dmgValue;
-            yield return new WaitForSeconds(drainRate);  // wait 1 second
+            yield return new WaitForSeconds(drainRate);  //wait 1 second
 
         }
+    }
 
+    private void GetCurrNote()
+    {
+        Debug.LogError("Need to implement finding current Note!");
+        UnityEditor.EditorApplication.isPlaying = false;
+        return;
+    }
+
+    public void BeginTouch(int lane)
+    {
+        Debug.Log("Touch began at lane " + lane);
+        SoundManager.instance.playSound(TapSound, transform, volume);
+
+        GetCurrNote();
+        noteStartLane = lane;
+        noteStartPos = noteCurrPos;
+    }
+
+    private bool WasNoteHit(float inputPos, float expectedPos)
+    {
+        return Mathf.Abs(inputPos - expectedPos) <= maxAllowedNoteDelta;
+    }
+
+    //TODO: Calculate score, streaks, and delete properly hit note
+    public void EndTouch(int lane, bool isHold)
+    {
+        Debug.Log("Touch ended at lane " + lane);
+
+        //Wrong lane or didn't stay in lane
+        if (lane != lowestNote.lane || lane != noteStartLane)
+        {
+            currHP -= dmgValue;
+            return;
+        }
+
+        if (isHold && lowestNote.type == NoteType.Hold)
+        {
+            //Ensure note was started and ended at proper times, within the allowed delta
+            if (!WasNoteHit(noteStartPos, timingLine.position.y) || !WasNoteHit(noteCurrPos, timingLine.position.y))
+            {
+                currHP -= dmgValue;
+                return;
+            }
+        }
+        else if (!isHold && lowestNote.type == NoteType.Tap)
+        {
+            //Ensure note is hit at proper time, within the allowed delta
+            if (!WasNoteHit(noteCurrPos, timingLine.position.y))
+            {
+                currHP -= dmgValue;
+                return;
+            }
+        }
+        else 
+        {
+            currHP -= dmgValue;
+            return;
+        }
     }
 }
